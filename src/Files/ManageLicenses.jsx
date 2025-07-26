aa11a3ec70c130c865306a86f015a7ec58017635
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-26 12:59:05 +0500

    responsive the manage licenses page of the application

--- src/Files/ManageLicenses.jsx
+++ src/Files/ManageLicenses.jsx
@@ -5,38 +5,38 @@ import { faFire, faCheck } from '@fortawesome/free-solid-svg-icons';
 const ManageLicenses = () => {
     return (
         <div className='px-4  font-roboto'>
-            <div className='mt-20  bg-white py-5 px-7 rounded-xl shadow-lg'>
-                <div className='w-4/12'>
-                    <h1 className='text-2xl'>Current Subscription</h1>
+            <div className='mt-20  bg-white py-5 lg:px-7 px-4 rounded-xl shadow-lg'>
+                <div className='lg:w-4/12 sm:w-8/12 w-full'>
+                    <h1 className='lg:text-2xl text-xl'>Current Subscription</h1>
                     <div className='flex mt-8 items-center justify-between'>
-                        <h1 className='text-3xl font-semibold'> <span className='text-orange-500'><FontAwesomeIcon icon={faFire} /></span> Pro</h1>
-                        <p className='text-sm font-semibold bg-black text-white px-3 py-1 tracking-widest rounded-full'>POPULAR</p>
+                        <h1 className='lg:text-3xl sm:text-2xl text-xl  font-semibold'> <span className='text-orange-500'><FontAwesomeIcon icon={faFire} /></span> Pro</h1>
+                        <p className='text-sm font-semibold bg-black text-white lg:px-3 px-2 py-1 tracking-widest rounded-full'>POPULAR</p>
                     </div>
                     <p className='mt-5 text-gray-500 leading-loose tracking-wider'>Perfect plan for professionals!</p>
                     <p className='text-gray-500 tracking-wider'> <span className='text-3xl font-bold text-black leading-loose'>$12</span> /month</p>
                     <p className='text-gray-800 tracking-wider py-3'> For professional only! Start arranging your expenses with our best templates.</p>
                     <div className='mt-3 space-y-4'>
-                        <p className='flex items-center gap-2 text-lg font-medium'>
+                        <p className='flex items-center gap-2 sm:text-lg text-sm font-medium'>
                             <span className='w-6 h-6 flex items-center justify-center bg-green-400 text-white rounded-full'><FontAwesomeIcon icon={faCheck} /></span>
                             <p>30+ Domains</p>
                         </p>
-                        <p className='flex items-center gap-2 text-lg font-medium'>
+                        <p className='flex items-center gap-2 sm:text-lg text-sm font-medium'>
                             <span className='w-6 h-6 flex items-center justify-center bg-green-400 text-white rounded-full'><FontAwesomeIcon icon={faCheck} /></span>
                             <p>Unlimited workspace</p>
                         </p>
-                        <p className='flex items-center gap-2 text-lg font-medium'>
+                        <p className='flex items-center gap-2 sm:text-lg text-sm font-medium'>
                             <span className='w-6 h-6 flex items-center justify-center bg-green-400 text-white rounded-full'><FontAwesomeIcon icon={faCheck} /></span>
                             <p>Collaborative workspace</p>
                         </p>
-                        <p className='flex items-center gap-2 text-lg font-medium'>
+                        <p className='flex items-center gap-2 sm:text-lg text-sm font-medium'>
                             <span className='w-6 h-6 flex items-center justify-center bg-green-400 text-white rounded-full'><FontAwesomeIcon icon={faCheck} /></span>
                             <p>Sharing permission</p>
                         </p>
-                        <p className='flex items-center gap-2 text-lg font-medium'>
+                        <p className='flex items-center gap-2 sm:text-lg text-sm font-medium'>
                             <span className='w-6 h-6 flex items-center justify-center bg-green-400 text-white rounded-full'><FontAwesomeIcon icon={faCheck} /></span>
                             <p>Admin tools</p>
                         </p>
-                        <p className='flex items-center gap-2 text-lg font-medium'>
+                        <p className='flex items-center gap-2 sm:text-lg text-sm font-medium'>
                             <span className='w-6 h-6 flex items-center justify-center bg-green-400 text-white rounded-full'><FontAwesomeIcon icon={faCheck} /></span>
                             <p>100+ integrations</p>
                         </p>
