c0cc3a201469e4cfc7c6ef2d78f8c20128ed8869
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-26 12:22:24 +0500

    responsive the navbar of the application of the website

--- src/compnents/navbar.jsx
+++ src/compnents/navbar.jsx
@@ -6,15 +6,15 @@ import logo from '../assets/logo.png'
 const navbar = () => {
     return (
         <div className='flex fixed z-10 w-full items-center justify-between font-roboto bg-gray-50 shadow-xl h-16 px-4'>
-            <div className='w-8/12 flex items-center gap-28'>
-                <img src={logo} alt="logo" className='w-2/12' />
-                <h1 className='text-2xl font-light'>
+            <div className='lg:w-8/12 w-full flex items-center lg:justify-normal justify-between lg:gap-28 sm:gap-16 gap-6'>
+                <img src={logo} alt="logo" className='lg:w-2/12 sm:w-3/12 w-5/12' />
+                <h1 className='lg:text-2xl sm:text-xl text-sm -light'>
                     Certificate Tracker
                 </h1>
             </div>
-            <div className='w-4/12 relative'>
+            <div className='lg:w-4/12 lg:block hidden  relative'>
                 <input type="text" className='w-full h-12 border rounded-md focus:outline-none px-3 text-lg ' placeholder='Search Certificate' name="" id="" />
-                <button className='absolute right-4 text-xl top-2 text-orange-500'>
+                <button className='absolute right-4 text-xl lg:block hidden top-2 text-orange-500'>
                     <FontAwesomeIcon icon={faSearch} />
                 </button>
             </div>
